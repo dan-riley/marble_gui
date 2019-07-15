13218ab0ab36e1fc9efddeca42602b807c6a32b8
Sam Gillam <gillamkid@gmail.com>
2019-07-15 16:09:35 -0600

    added some notes

--- scripts/gui_launch.sh
+++ scripts/gui_launch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+# the argument should be the path to your_ws/marble/marble_gui/src
 cd $1
+
 npm start
--- scripts/test_server_launch.sh
+++ scripts/test_server_launch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+# this argument should be the path to your_ws/marble/marble_gui/test/test_scoring_server_stix1
 cd $1
+
 docker-compose up --build
