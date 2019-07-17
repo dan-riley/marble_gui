3f37b71caa26928c6030081bccd3e449b585c379
sam gillam <gillamkid@gmail.com>
2019-07-17 13:52:16 -0600

    QA: removed hard coded reference to directory

--- scripts/gui_launch.sh
+++ scripts/gui_launch.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-#! Directory used for base station testing at SSCI
-# cd $HOME/subt_ws/src/subT_ssci/ros_gui/src/
-
-#! Directory used for base station on Nook
-# cd $HOME/catkin_ws/src/ros_gui/src/
-# source ~/catkin_ws/devel/setup.bash
-{
-    source ~/gui_ws/devel/setup.bash
-
-} || {
-
-    echo -e "\e[31;43mThere is not a setup file in this ws. Change the ws directory inside gui_launch.sh to make this script work.\e[0m"
-    exit 3
-}
-roscd marble_gui/src
-
+# $1 = your_ws/src/marble/marble_gui/src
+cd $1
 npm start
