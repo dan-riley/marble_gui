686ff2f7550098110517056b541a8930026433f8
NikolaasBender <nibe6600@colorado.edu>
2020-01-15 13:32:47 -0700

    dev for interactive markers

---
+++ scripts/launch_local_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ~/catkin_ws/devel/setup.bash
+
+# This is for statring the darpa test server
+gnome-terminal -e 'sh -c "cd ~/test_scoring_server && docker-compose up --build; exec bash"' &
+
+sleep 5
+
+# start the marble gui
+gnome-terminal -e 'sh -c "roslaunch marble_gui marble_gui.launch; exec bash"' &
+
+sleep 5
+
+# play the rosbag of deployment 3
+# gnome-terminal -e 'sh -c "rosbag play ~/catkin_ws/bagfiles/2019-08-20-14-01-16.bag; exec bash"' &
+
+# gnome-terminal -e 'sh -c "roslaunch `rospack find teleop_twist_joy`/launch/teleop.launch"' &
+
