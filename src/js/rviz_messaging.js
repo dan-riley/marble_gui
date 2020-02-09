a0fc410387325bc383d13d001d2f8c8369a6436c
NikolaasBender <nibe6600@colorado.edu>
2020-02-09 10:33:52 -0700

    grouping functions into different files

--- src/js/rviz_messaging.js
+++ src/js/rviz_messaging.js
@@ -1,41 +1,14 @@
-// This will listen to and process the updates from the marker server to update the gui
-function listen_to_markers(){
-    var listener = new ROSLIB.Topic({
-        ros : ros,
-        name : '/mkr_srv_talkback',
-        messageType : 'marble_gui/ArtifactTransport'
-      });
-    
-      listener.subscribe(function(message) {
-        // Kick this over to update fused artifacts
-        update_fused_artifact(message);
-        // listener.unsubscribe();
-      });
-}
-
-// This sends a fused artifact to the marker server
-function send_fused_update(artifact, id){
-    // Important to catch these null artifacts
-    if(artifact != undefined){
-        console.log("sending fused artifact to server: " + artifact)
-        var fused_pub = new ROSLIB.Topic({
-            ros: ros,
-            // You should probably make this actually work, it super doesn't now and current nick is too tired to deal with it
-            name: `/gui/fused_artifact`,
-            // Probably change this to a custom message
-            messageType: "marble_gui/ArtifactTransport"
-        });
-        // console.log(artifact)
-        // Use the pose to make life easy. just neglect the orientation stuff
-        var pose = new ROSLIB.Message({
-            object_class: artifact.obj_class,
-            id: id,
-            position: artifact.position,
-            origin: "gui"
-        });
-        //   console.log(pose)
-        fused_pub.publish(pose)
-    }
+// This sends the goal pose from RVIZ to the correct robot
+function listen_to_pose(){
+    var pose_listener = new ROSLIB.Topic({
+        ros: ros,
+        name: '/robot_to_goal',
+        messageType: 'geometry_msgs/Pose'
+    });
+   
+    pose_listener.subscribe(function (message){
+        goal_pose = message
+    });
 }
 
 function publish_goal(robot){
@@ -51,29 +24,47 @@ function publish_goal(robot){
     }
 }
 
-// This sends the goal pose from RVIZ to the correct robot
-function listen_to_pose(){
-    var pose_listener = new ROSLIB.Topic({
-        ros: ros,
-        name: '/robot_to_goal',
-        messageType: 'geometry_msgs/Pose'
-    });
-   
-    pose_listener.subscribe(function (message){
-        goal_pose = message
-    });
+// This sends a fused artifact to the marker server
+function send_fused_update(artifact, id, old_id) {
+    // Important to catch these null artifacts
+    if (artifact != undefined) {
+        if (old_id)
+            console.log("Updating fused artifact " + old_id + " with " + id)
+        else
+            console.log("Sending new fused artifact to server: " + id)
+
+        var fused_pub = new ROSLIB.Topic({
+            ros: ros,
+            // You should probably make this actually work, it super doesn't now and current nick is too tired to deal with it
+            name: `/gui/fused_artifact`,
+            // Probably change this to a custom message
+            messageType: "marble_gui/ArtifactTransport"
+        });
+        // console.log(artifact)
+        // Use the pose to make life easy. just neglect the orientation stuff
+        var pose = new ROSLIB.Message({
+            object_class: artifact.obj_class,
+            id: id,
+            old_id: old_id,
+            position: artifact.position,
+            origin: "gui"
+        });
+        //   console.log(pose)
+        fused_pub.publish(pose)
+    }
 }
 
-
-// This moves the goal to the robot
-function goal_to_robot(robot){
-    // how get pose of robot?
-
-    var fused_pub = new ROSLIB.Topic({
-        ros: ros,
-        // You should probably make this actually work, it super doesn't now and current nick is too tired to deal with it
-        name: `/gui/goal_to_robot`,
-        // Probably change this to a custom message
-        messageType: "geometry_msgs/Pose"
-    });
+// This will listen to and process the updates from the marker server to update the gui
+function listen_to_markers(){
+    var listener = new ROSLIB.Topic({
+        ros : ros,
+        name : '/mkr_srv_talkback',
+        messageType : 'marble_gui/ArtifactTransport'
+      });
+    
+      listener.subscribe(function(message) {
+        // Kick this over to update fused artifacts
+        update_fused_artifact(message);
+        // listener.unsubscribe();
+      });
 }
