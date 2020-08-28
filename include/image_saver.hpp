11129ce438eec983d9e1ad6280ab04635a6d2b6d
nikolaasbender <nibe6600@colorado.edu>
2020-08-28 16:38:54 -0600

    bug fix with image saving

--- include/image_saver.hpp
+++ include/image_saver.hpp
@@ -19,7 +19,7 @@ using namespace std;
 // This is to organize image data
 struct image{
     string robot_name;
-    float id;
+    string artifact_id;
 
     // image(string name_, float id_){
     //     robot_name = name_;
@@ -34,12 +34,13 @@ class ImageSaver{
         string img_dir_;
         ros::NodeHandle nh_;
         ros::Subscriber img_sub;
-
+        
     public:
         ImageSaver(ros::NodeHandle* nh, string robot_name, string comms_prefix);
         ~ImageSaver();
         bool imageExists(string id);
         void imageWriter(marble_artifact_detection_msgs::ArtifactImg image);
+        string name_;
 };
 
 #endif
